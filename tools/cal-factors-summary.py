0068c0d4f5ba31ffc70cab32d23890d83bc35970
BaptisteVandecrux <b.vandecrux@gmail.com>
2022-08-14 09:35:48 +0200

    Moved some folders to clear root directories
    - changed NSWR to NR (fix #79)
    - removed ISWR and OSWR from E-GRIP L0 header because there are no instrument connected to those channels
    - added a compilation of calibration factors and mount info
    - corrected pressure offset at Dye-2 and removing adjustment that had been used as a fix  (solving #20 in a different way)
    - corrected pressure offset at JAR1 and removing adjustment that had been used as a fix (solving #15 in a different way)
    - At Tunu-N changed the calibration coefficients to match QCheck values (#89)

---
+++ tools/cal-factors-summary.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+@author: bav@geus.dk
+
+tip list:
+    %matplotlib inline
+    %matplotlib qt
+    import pdb; pdb.set_trace()
+"""
+import pandas as pd
+import numpy as np 
+from nead import read
+import os 
+list_dir = os.listdir('.')
+
+for file in list_dir[1:-3]:
+    df_cal = read(file)
+    # TCAir, CS500 ,WindSpeed
+    try:
+        print([file[3:-4]] + \
+              [df_cal[var].scale_factor for var in ['TA1', 'TA3', 'VW1', 'ISWR', 'OSWR','NSWR']] \
+            + [df_cal['NSWR'].scale_factor_neg] \
+                + [df_cal['P'].add_value] \
+                + [df_cal['NSWR'].add_value])
+    except:
+        pass
+
+# df_all.to_csv('saved_calibration_factors.csv')
